14b956405bd8c9bcd527984352d2c8388a6f0aa8
0biin0 <infinite402@gmail.com>
2024-12-08 17:30:52 +0900

    디테일 스타일

--- Popspot/src/main/popspot/src/pages/event/EventDetail.js
+++ Popspot/src/main/popspot/src/pages/event/EventDetail.js
@@ -21,7 +21,7 @@ function EventDetail() {
   const [reviews, setReviews] = useState(null);
   const navigate = useNavigate();
   const [likeNo,setLikeNo] = useState(null);
-  const	userId = sessionStorage.userId;
+  const	userId = sessionStorage.userId||"";
   
   const [likes,setLikes] = useState(false);
   useEffect(() => {
@@ -68,11 +68,12 @@ function EventDetail() {
 	}
 
   const like = () => {
+	  if(userId!==""){
 	 axios.post(`/api/event/like/${no}/${userId}`)
 	 .then(()=>{
 		 setLikes(p =>!p)
 		 setLikeNo(p =>likes ? p-1 : p+1 )
-	 })
+	 })}
   }
 
   return (
