bd4525f4fe66234666ded5e17672d30e7a6d9447
ksks0911 <k.kimsoul0911@gmail.com>
2024-12-09 00:53:50 +0900

    1208_수정

--- Popspot/src/main/popspot/src/pages/support/UserSupport.js
+++ Popspot/src/main/popspot/src/pages/support/UserSupport.js
@@ -6,115 +6,125 @@ import { RightFloatSpan } from "../styles/FaqStyle";
 
 
 function UserSupport() {
-	const [width, setWidth] = useState(window.innerWidth);
-	const userId = sessionStorage.getItem('userId') || '';
-	const userPermission = sessionStorage.getItem('permissions') || '';
-	
-	useEffect(() => {
-		const getNowWidth = () => {
-			setWidth(window.innerWidth)
-		};
-		
-		window.addEventListener('resize', getNowWidth);
-		
-		return () => {
-			window.removeEventListener('resize', getNowWidth);
-		}
-	})
-	
-	
-	
-	return (
-		<ContentContainer width={width}>
-			<ContentVerticalSpan>
-				<ContentHorizontalBar width={'95%'} borderpixel={2} />				
-					<UserSupportList userId={userId} userPermission={userPermission}/>
-				<ContentHorizontalBar width={'95%'} borderpixel={2} />
-			</ContentVerticalSpan>
-		</ContentContainer>
-	)
+   const [width, setWidth] = useState(window.innerWidth);
+   const userId = sessionStorage.getItem('userId') || '';
+   const userPermission = sessionStorage.getItem('permissions') || '';
+   
+   useEffect(() => {
+      const getNowWidth = () => {
+         setWidth(window.innerWidth)
+      };
+      
+      window.addEventListener('resize', getNowWidth);
+      
+      return () => {
+         window.removeEventListener('resize', getNowWidth);
+      }
+   })
+   
+   
+   
+   return (
+      <ContentContainer width={width}>
+         <ContentVerticalSpan>
+            <ContentHorizontalBar width={'95%'} borderpixel={2} />            
+               <UserSupportList userId={userId} userPermission={userPermission}/>
+            <ContentHorizontalBar width={'95%'} borderpixel={2} />
+         </ContentVerticalSpan>
+      </ContentContainer>
+   )
 }
 
 function UserSupportList({userId, userPermission}) {
-	const [sList, setSList] = useState([]);
-	
-	useEffect(() => {
-		axios
-			.get('/api/support/user-support')
-			.then(response => setSList(response.data));
-	}, []);
-	
-	const navigate = useNavigate(); 
-	
-	return (
-		<ContentVerticalSpan style={{alignItems: 'center'}}>
-			<h2 style={{margin: '0 auto'}}>1:1 고객 문의</h2>
-			<RightFloatSpan>
-			{userId !== null ?
-					<input 
-						type="button" 
-						value={"문의사항 등록"}
-						style={{backgroundColor: '#006EB9',
-								color: 'white',
-								fontSize:'15px',  
-								padding: '5px',
-								marginLeft: '800px', 
-								borderColor: 'transparent'			
-						      }}					
-						onClick={() => navigate('/support/usersupport/register')}
-					/>:
-					<span style={{color: '#000'}}>로그인 후에 문의 가능합니다!</span> 
-			}
-			</RightFloatSpan>				
-			<ContentHorizontalBar width={'100%'} borderpixel={2} />
-			{sList.map((e, i) => {
-				if(e.secret !== 1){
-					return (
-						<ContentHorizontalSpan key={i} redirect={'y'} onClick={() => navigate(`/support/usersupport/detail/${e.supportNo}`)}>
-							<span className="no">{sList.length - i}</span>
-							<span className="type">{getType(e.type)}</span>
-							<span className="secret"/>
-							<span className="userId">{e.userId}</span>
-							<span className="title">{e.title}</span>
-						</ContentHorizontalSpan>
-					);
-				}
-				
-				if(e.userId === userId || userPermission.includes('admin')){
-					return(
-						<ContentHorizontalSpan key={i} redirect={'y'} onClick={() => navigate(`/support/usersupport/detail/${e.supportNo}`)}>
-							<span className="no">{sList.length - i}</span>
-							<span className="type">{getType(e.type)}</span>
-							<span className="secret"> 🔓 </span>
-							<span className="userId">{e.userId}</span>
-							<span className="title">{e.title}</span>
-						</ContentHorizontalSpan>
-					);
-				}
-				
-				return (
-					<ContentHorizontalSpan redirect={'n'}>
-						<span className="no">{i+1}</span>
-						<span className="type">{getType(e.type)}</span>
-						<span className="secret"> 🔒 </span>
-						<span className="userId">{e.userId}</span>
-						<span className="title">비공개 문의사항입니다.</span>
-					</ContentHorizontalSpan>
-				)
-			})}
-		</ContentVerticalSpan>
-	)
+   const [sList, setSList] = useState([]);
+   
+   useEffect(() => {
+      axios
+         .get('/api/support/user-support')
+         .then(response => setSList(response.data));
+   }, []);
+   
+   const navigate = useNavigate();
+   
+   const showDetail = (url) => {
+      if(userId === ''){
+         alert("로그인 후 확인 가능합니다.");
+         return;
+      }
+      
+      navigate(url);
+      return;
+   } 
+   
+   return (
+      <ContentVerticalSpan style={{alignItems: 'center'}}>
+         <h2 style={{margin: '0 auto'}}>1:1 고객 문의</h2>
+         <RightFloatSpan>
+         {userId !== '' ?
+               <input 
+                  type="button" 
+                  value={"문의사항 등록"}
+                  style={{backgroundColor: '#006EB9',
+                        color: 'white',
+                        fontSize:'15px',  
+                        padding: '5px',
+                        marginLeft: '800px', 
+                        borderColor: 'transparent'         
+                        }}               
+                  onClick={() => navigate('/support/usersupport/register')}
+               />:
+               <span style={{color: '#000'}}>로그인 후에 문의 가능합니다!</span> 
+         }
+         </RightFloatSpan>            
+         <ContentHorizontalBar width={'100%'} borderpixel={2} />
+         {sList.map((e, i) => {
+            if(e.secret !== 1){
+               return (
+                  <ContentHorizontalSpan key={i} redirect={'y'} onClick={() => showDetail(`/support/usersupport/detail/${e.inquiryNo}`)}>
+                     <span className="no">{sList.length - i}</span>
+                     <span className="type">{getType(e.type)}</span>
+                     <span className="secret"/>
+                     <span className="userId">{e.userId}</span>
+                     <span className="inqTitle">{e.inqTitle}</span>
+                  </ContentHorizontalSpan>
+               );
+            }
+            
+            if(e.userId === userId || userPermission.includes('admin')){
+               return(
+                  <ContentHorizontalSpan key={i} redirect={'y'} onClick={() => navigate(`/support/usersupport/detail/${e.inquiryNo}`)}>
+                     <span className="no">{sList.length - i}</span>
+                     <span className="type">{getType(e.type)}</span>
+                     <span className="secret"> 🔓 </span>
+                     <span className="userId">{e.userId}</span>
+                     <span className="title">{e.inqTitle}</span>
+                  </ContentHorizontalSpan>
+               );
+            }
+            
+            return (
+               <ContentHorizontalSpan redirect={'n'}>
+                  <span className="no">{sList.length - i}</span>
+                  <span className="type">{getType(e.type)}</span>
+                  <span className="secret"> 🔒 </span>
+                  <span className="userId">{e.userId}</span>
+                  <span className="title">비공개 문의사항입니다.</span>
+               </ContentHorizontalSpan>
+            )
+         })}
+      </ContentVerticalSpan>
+   )
 }
 
 function getType(type){
-	switch(type){
-		case 1:
-			return '로그인';
-		case 2:
-			return '회원가입';
-		default:
-			return '기타'
-	}
+   switch(type){
+      case 1:
+         return '로그인';
+      case 2:
+         return '회원가입';
+      default:
+         return '기타'
+   }
 }
 
 
